20af0b3d1fd9ab7ec436d752ccb0686c7c63fb24
ReazFaruk <57425519+ReazFaruk@users.noreply.github.com>
2020-11-22 17:27:03 +0600

    Add files via upload

---
+++ Reg form/deliveryman reg.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $phone = test_input($_POST["phone"]);
+  $address = test_input($_POST["address"]);
+  $gender = test_input($_POST["gender"]);
+ 
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>Customer Registration</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Phone: <input type="text" name="phone">
+  <br><br>
+  Address: <input type="text" name="address">
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+
+  Vehicle Number: <input type="text" name="vehicle number">
+  <br><br>
+   <div class="form-group">
+<label for="birthdate">Date of Birth: </label>
+<input type="date" id="birthday" name="birthday"><br>
+</div><br><br>
+  <input type="submit" name="submit" value="Submit">  
+
+  <div class="container signin">
+    <p>Already have an account? <a href="#">Sign in</a>.</p>
+  </div>
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
